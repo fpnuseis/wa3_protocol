086a4b413f2df23f14c5ff49a4be96d0b13b5e8e
kuma_jyj <yjjung@cnlab.cse.cau.ac.kr>
2018-04-03 17:52:14 +0900

    2nd commint

--- src/Protocol.java
+++ src/Protocol.java
@@ -20,9 +20,9 @@ public class Protocol {
    * REGISTER(0x01) : 클라이언트 접속
    * EXIT(0x02) : 클라이언트 접속 종료
    */
-  public static final byte MESSAGE = 0x00;
-  public static final byte CONNECT = 0x01;
-  public static final byte EXIT = 0x02;
+  public static final byte[] MESSAGE = new byte[] {0x00};
+  public static final byte[] CONNECT = new byte[] {0x01};
+  public static final byte[] EXIT = new byte[] {0x02};
 
   /**
    * header(magicNumber, opcode, length) 초기화
@@ -52,16 +52,20 @@ public class Protocol {
    * set body
    * length의 값에 맞춰 allocate를 수행한 후, body를 set
    */
-  public void setBody(ByteBuffer body) {
+  public void setBody(byte[] body) {
+    byte[] len = new byte[4];
+    header.get(len, 5, 4);
+    ByteBuffer wrappedLength = ByteBuffer.wrap(len);
+    this.body = ByteBuffer.allocate(wrappedLength.getInt());
     this.body.put(body);
   }
-  
-  //
-  //public boolean isCompleteMessage() {
-  //  return true;
-  //}
 
+  /**
+   * CompleteMessage
+   */
   public ByteBuffer[] getCompleteMessage() {
+    header.flip();
+    body.flip();
     return new ByteBuffer[] {header, body};
   }
 }
