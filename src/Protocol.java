718ffd1ba7a2a4832ff9fe928e7d4c9222fd2536
yhkim <fpnuseis@hanyang.ac.kr>
2018-04-04 10:32:01 +0900

    bytebuffer issue 수정

--- src/Protocol.java
+++ src/Protocol.java
@@ -1,4 +1,5 @@
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 /**
  * 프로토콜 구성 영역
@@ -7,7 +8,6 @@ import java.nio.ByteBuffer;
  * length : 4byte
  * body : length 값만큼의 byte
  * 위 값에 따라 allocate 수행하며, body는 setBody 메소드 호출시 allocate
- *
  * @author yhkim
  */
 public class Protocol {
@@ -25,7 +25,7 @@ public class Protocol {
   public static final byte[] EXIT = new byte[] {0x02};
 
   /**
-   * header(magicNumber, opcode, length) 초기화
+   * header(magicNumber, opcode, length)를 default 값으로 초기화
    * body의 경우 length에 따라 크기가 다르기 때문에 초기화 x
    */
   Protocol() {
@@ -36,36 +36,47 @@ public class Protocol {
 
   /**
    * set Opcode
+   * @param opcode
+   * offset을 지정하는 put 메소드가 byte[] 형만을 지원하여 byte[] 형태로 받음
    */
   public void setOpcode(byte[] opcode) {
-    header.put(opcode, 4, 1);
+    header.position(4);
+    header.put(opcode, 0, 1);
   }
 
   /**
    * set Length
+   * @param length
+   * offset을 지정하는 put 메소드가 byte[] 형만을 지원하여 byte[] 형태로 받음
    */
   public void setLength(byte[] length) {
-    header.put(length, 5, 4);
+    header.position(5);
+    header.put(length, 0, 4);
   }
 
   /**
-   * set body
-   * length의 값에 맞춰 allocate를 수행한 후, body를 set
+   * set Body
+   * @param body
+   * header bytebuffer에서 length를 가져온 후, length 값에 맞게 body bytebuffer를 allocate
    */
   public void setBody(byte[] body) {
     byte[] len = new byte[4];
-    header.get(len, 5, 4);
+    header.position(5);
+    header.get(len, 0, 4);
     ByteBuffer wrappedLength = ByteBuffer.wrap(len);
     this.body = ByteBuffer.allocate(wrappedLength.getInt());
     this.body.put(body);
   }
 
   /**
-   * CompleteMessage
+   * header와 body로 구성된 Bytebuffer Array를 리턴하는 메소드
+   * SocketChannel의 write 메소드는 ByteBuffer Array 또한 write를 지원하기 때문에,
+   * ByteBuffer를 concat하지 않아도 됨
+   * @return ByteBuffer[] {header, body}
    */
   public ByteBuffer[] getCompleteMessage() {
-    header.flip();
-    body.flip();
+    header.rewind();
+    body.rewind();
     return new ByteBuffer[] {header, body};
   }
 }
